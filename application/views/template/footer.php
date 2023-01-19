5dfc5d98b51e779d18cc763ab92a5bd010197208
aprilliatrilestari805@gmail.com <aprilliatrilestari@gmail.com>
2023-01-19 19:50:42 +0700

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.0
+    </div>
+    <strong> &copy; 2022 TimSA02 <a href="#">Absensi Digital</a>.</strong> Template by Admin LTE.
+</footer>
